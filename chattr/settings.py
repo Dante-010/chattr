25c46db4d6d3345caeb3ab3eb426bad14b621e8f
Dante-010 <programmingdante@gmail.com>
2020-12-12 23:38:28 -0300

    Add .gitignore and README

--- chattr/settings.py
+++ chattr/settings.py
@@ -67,7 +67,7 @@ WSGI_APPLICATION = 'chattr.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/mnt/data/databases/chattr/db.sqlite3',
         'TEST': {
             'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
         }
