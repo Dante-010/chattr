4576c208fc11b685ff872e4f569b1b0388be3ca5
Dante-010 <programmingdante@gmail.com>
2020-12-14 15:08:27 -0300

    Redesigned index.html and added ChatRoom model

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from .models import ChatRoom
+
 def index(request):
-    return render(request, 'chat/index.html')
+    return render(request, 'chat/index.html', {
+        'chat_rooms': ChatRoom.objects.all()
+    })
 
 def room(request, room_name):
     return render(request, 'chat/room.html', {
