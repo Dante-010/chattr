c2ceb31df2953996652083ec46de9835d98cdf90
Dante Culaciati <programmingdante@gmail.com>
2021-01-12 01:36:01 -0300

    Changed settings.py

--- chattr/settings.py
+++ chattr/settings.py
@@ -1,7 +1,6 @@
 import django
 django.setup()
 
-from chattr.secrets import *
 from pathlib import Path
 import os
 
@@ -13,12 +12,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY located in secrets.py
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 ADMINS = [('Dante', 'programmingdante@gmail.com'),]
 
