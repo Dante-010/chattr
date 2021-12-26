f05d71c397f4c06b49b442229cde9ae18aa09d2b
Dante Culaciati <programmingdante@gmail.com>
2021-12-26 02:01:38 -0300

    Added 'localhost' to ALLOWED_HOSTS.

--- chattr/settings.py
+++ chattr/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS=['nginx']
+ALLOWED_HOSTS=['nginx', 'localhost']
 
 ADMINS = [('Dante', 'programmingdante@gmail.com'),]
 
