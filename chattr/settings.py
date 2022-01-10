a4ebacbcdbdefb35ba2dd950e3598a980d636de2
Dante Culaciati <programmingdante@gmail.com>
2022-01-10 19:56:10 -0300

    Fixed test database in settings.py.

--- chattr/settings.py
+++ chattr/settings.py
@@ -75,7 +75,10 @@ DATABASES = {
     },
     'development': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+        }
     }
 }
 
