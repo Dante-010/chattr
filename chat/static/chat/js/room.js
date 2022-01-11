45caa0cc3a4da8859bb33733dac90ffaa96973d5
Dante Culaciati <programmingdante@gmail.com>
2022-01-10 22:49:18 -0300

    Reformatted website.

--- chat/static/chat/js/room.js
+++ chat/static/chat/js/room.js
@@ -20,15 +20,11 @@ chatSocket.onclose = function(e) {
 document.querySelector('#chat-message-input').focus();
 document.querySelector('#chat-message-input').onkeyup = function(e) {
     if (e.keyCode === 13) {  // enter, return
-        document.querySelector('#chat-message-submit').click();
-    }
-};
-
-document.querySelector('#chat-message-submit').onclick = function(e) {
-    const messageInputDom = document.querySelector('#chat-message-input');
+        const messageInputDom = document.querySelector('#chat-message-input');
     const message = messageInputDom.value;
     chatSocket.send(JSON.stringify({
         'message': message
     }));
     messageInputDom.value = '';
+    }
 };
